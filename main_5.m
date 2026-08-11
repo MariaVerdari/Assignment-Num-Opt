b782b7d30a8bc5ed779e49ec7f472145ef3051c9
MariaVerdari <s310724@studenti.polito.it>
2026-08-11 16:07:12 +0200

    grafico

--- main_5.m
+++ main_5.m
@@ -243,117 +243,34 @@ for i = 1:size(X, 1)
     end
 end
 
-
 % better levels
 z_min = min(Z(:)); 
 z_max = max(Z(:));
 levels = logspace(log10(z_min + 0.1), log10(z_max), 80) - 0.1; 
 
-contour(X, Y, Z, levels); 
+contour(X, Y, Z, levels, 'LineColor', [0.7 0.7 0.7]); 
 hold on;
 
-plot(xBigSeq_M{1}(1,:), xBigSeq_M{1}(2,:), 'r.-', 'DisplayName', 'Start 1');
-plot(xBigSeq_M{2}(1,:), xBigSeq_M{2}(2,:), 'b.-', 'DisplayName', 'Start 2');
-plot(xBigSeq_M{3}(1,:), xBigSeq_M{3}(2,:), 'g.-', 'DisplayName', 'Start 3');
-plot(xBigSeq_M{4}(1,:), xBigSeq_M{4}(2,:), 'm.-', 'DisplayName', 'Start 4');
-plot(xBigSeq_M{5}(1,:), xBigSeq_M{5}(2,:),'c.-', 'DisplayName', 'Start 5');
-plot(xBigSeq_M{6}(1,:), xBigSeq_M{6}(2,:), 'k.-', 'DisplayName', 'Start 6');
+c1 = [0.000, 0.447, 0.741]; 
+c2 = [0.850, 0.325, 0.098]; 
+c3 = [0.466, 0.674, 0.188]; 
+c4 = [0.494, 0.184, 0.556]; 
+c5 = [0.929, 0.694, 0.125]; 
+c6 = [0.850, 0.000, 0.150];
 
-title('Top view of the function and sequence paths modified 5) (n=2)');
+lw = 1.5; 
+ms = 12;  
+
+plot(xBigSeq_M{1}(1,:), xBigSeq_M{1}(2,:), '.-', 'Color', c1, 'LineWidth', lw, 'MarkerSize', ms, 'DisplayName', 'Start 1');
+plot(xBigSeq_M{2}(1,:), xBigSeq_M{2}(2,:), '.-', 'Color', c2, 'LineWidth', lw, 'MarkerSize', ms, 'DisplayName', 'Start 2');
+plot(xBigSeq_M{3}(1,:), xBigSeq_M{3}(2,:), '.-', 'Color', c3, 'LineWidth', lw, 'MarkerSize', ms, 'DisplayName', 'Start 3');
+plot(xBigSeq_M{4}(1,:), xBigSeq_M{4}(2,:), '.-', 'Color', c4, 'LineWidth', lw, 'MarkerSize', ms, 'DisplayName', 'Start 4');
+plot(xBigSeq_M{5}(1,:), xBigSeq_M{5}(2,:), '.-', 'Color', c5, 'LineWidth', lw, 'MarkerSize', ms, 'DisplayName', 'Start 5');
+plot(xBigSeq_M{6}(1,:), xBigSeq_M{6}(2,:), '.-', 'Color', c6, 'LineWidth', lw, 'MarkerSize', ms, 'DisplayName', 'Start 6');
+
+title('Top view of the function and sequence paths (Modified Newton) (n=2)');
 xlabel('x_1');
 ylabel('x_2');
 legend('show');
-xlim([-2.5,1]);
+xlim([-2.5, 1]);
 ylim([-2.5, 0.5]);
-
-
-
-
-% TOP VIEW TRUNCATED
-figure;
-[X, Y] = meshgrid(linspace(-6, 2, 500), linspace(-3, 5, 500));
-Z = zeros(size(X));
-
-for i = 1:size(X, 1)
-    for j = 1:size(X, 2)
-        Z(i,j) = f([X(i,j); Y(i,j)]); 
-    end
-end
-
-% better levels
-z_min = min(Z(:)); 
-z_max = max(Z(:));
-levels = logspace(log10(z_min + 0.1), log10(z_max), 80) - 0.1; 
-
-contour(X, Y, Z, levels); 
-hold on;
-
-
-plot(xBigSeq_T{1}(1,:), xBigSeq_T{1}(2,:), 'r.-', 'DisplayName', 'Start 1');
-plot(xBigSeq_T{2}(1,:), xBigSeq_T{2}(2,:), 'b.-', 'DisplayName', 'Start 2');
-plot(xBigSeq_T{3}(1,:), xBigSeq_T{3}(2,:), 'g.-', 'DisplayName', 'Start 3');
-plot(xBigSeq_T{4}(1,:), xBigSeq_T{4}(2,:), 'm.-', 'DisplayName', 'Start 4');
-plot(xBigSeq_T{5}(1,:), xBigSeq_T{5}(2,:),'c.-', 'DisplayName', 'Start 5');
-plot(xBigSeq_T{6}(1,:), xBigSeq_T{6}(2,:), 'k.-', 'DisplayName', 'Start 6');
-
-title('Top view of the function and sequence paths (truncated 5) (n=2)');
-xlabel('x_1');
-ylabel('x_2');
-legend('show');
-xlim([-2.5,1]);
-ylim([-2.5, 0.5]);
-
-
-
-
-
-
- %% es 9.4
-
- 
-
-
-
-
-c1 = 1e-4;
-rho = 0.8;
-btmax = 50;
-x0 = [1, 0.5,0]; %alpha, phi, psi
-kmax = 5000;
-gradftol = 1e-4;
-
-F = @(x) x(1)*sin(wave_xx* x(2) +x(3) ) - wave_yy;
-%JF = @(x) x(1)*sin(wave_xx* x(2) +x(3) );
-
-% wave_X = [wave_xx, ones(size(wave_xx, 1),1)];
-% [wave_w,Fk, normFk,normgradfk, k, xseq, btseq] = gaussnewton(x0,F,JF,kmax,gradftol,c1, rho, btmax);
-% wave_w %w and b
-
-
-
-figure
-plot(sort(lin_xx), true_line(sort(lin_xx)));
-hold on
-plot(sort(lin_xx), lin_w(2) + lin_w(1).*sort(lin_xx));
-hold on
-scatter((lin_xx), lin_yy);
-hold off
-
-figure
-plot(sort(philin_xx), true_philine((sort(philin_xx))));
-hold on
-plot(sort(philin_xx), philin_w(2) + philin_w(1).*g(sort(philin_xx)));
-hold on
-scatter((philin_xx), philin_yy);
-hold off
-
-
-figure
-plot(sort(wave_xx), true_wave((sort(wave_xx))));
-hold on
-scatter((wave_xx), wave_yy);
-
-hold on
-plot(sort(wave_xx), (wave_w(3) + wave_w(2).*(sort(wave_xx)))*wave_w(1));
-
-hold off
